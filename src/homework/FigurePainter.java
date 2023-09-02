612da0edae482815ad1f454a4c79c3acde40ac63
mnajaryan <mnajaryan@list.ru>
2023-09-02 19:17:27 +0400

    my second project

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -48,11 +48,22 @@ public class FigurePainter {
         System.out.println("Ex 5");
         for (int i = 4; i >= 0; i--) {
             for (int j = 0; j < 5; j++) {
-                if (j < i) {
+                if (j < i){
                     System.out.print(" ");
                 }
-                if (j >= i) {
-                    System.out.print("**");
+                if (j >= i){
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i > j) {
+                    System.out.print(" ");
+                }
+                if (i <= j){
+                    System.out.print(" *");
                 }
             }
             System.out.println();
