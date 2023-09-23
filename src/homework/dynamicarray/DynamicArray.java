649c3d8995057ff20e09d994cd412cfb31dcde53
mnajaryan <mnajaryan@list.ru>
2023-09-23 20:38:04 +0400

    DynamicArrayTest

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -1,13 +1,9 @@
 package homework.dynamicarray;
 
 public class DynamicArray {
-    //սա մեր հիմնական մասիվն է, որտեղ պահելու ենք ավելացվող էլեմենտները
     private int[] array = new int[10];
-    //սա մեր մասիվի մեջ ավելացված էլեմենտների քանակն է
     private int size = 0;
 
-    //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
-    //և ավելացնենք
 
     public void add(int value) {
         if (array.length == size){
@@ -17,9 +13,6 @@ public class DynamicArray {
     }
 
 
-    //1․ ստեղծել հին մասիվից 10 էլեմենտ ավելի մեծ մասիվ
-    //2․ քցել հին մասիվի էլեմենտները նորի մեջ
-    //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
     private void extend() {
         int[] array2 = new int[array.length + 10];
         for (int i = 0; i < size; i++) {
@@ -29,8 +22,6 @@ public class DynamicArray {
     }
 
 
-    //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
-    // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
     public int getByIndex(int index) {
         if(index < size && index > -1){
             return array[index];
@@ -38,10 +29,75 @@ public class DynamicArray {
         return -1;
     }
 
-    //տպել մասիվի ավելացված էլեմենտները
     public void print() {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + ", ");
         }
     }
+
+    public void deleteByIndex(int index) {
+        if (index < 0 || index > array[size-1]){
+            System.out.println("false index");
+            return;
+        } else {
+            for (int i = index; i < size; i++) {
+                array[i - 1] = array[i];
+            }
+        }
+        size--;
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + ",");
+        }
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index > array[size-1]){
+            System.out.println("false index");
+            return;
+        }else{
+            array[index] = value;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + ",");
+        }
+    }
+
+    public void addElem(int index, int value) {
+        if (index < 0 || index > array[size-1]){
+            System.out.println("false index");
+            return;
+        }else{
+            size++;
+            for (int i = size; i > index; i--) {
+                int tmp = array[i - 1];
+                array[i - 1] = array[i];
+                array[i] = tmp;
+            }
+            array[index] = value;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + ",");
+        }
+    }
+
+    public boolean exists(int value) {
+        for (int i : array) {
+            if (value == array[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Վերադարձնել տրված value-ի ինդեքսը, եթե շատ կա տվյալ թվից, վերադարձնել առաջին ինդեքսը։
+//եթե չկա, -1
+    public int getIndexByValue(int value) {
+        for (int i : array) {
+            if (value == array[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
--- src/homework/dynamicarray/DynamicArrayDemo.java
+++ src/homework/dynamicarray/DynamicArrayDemo.java
@@ -4,13 +4,26 @@ public class DynamicArrayDemo {
     public static void main(String[] args) {
 
         DynamicArray ex1 = new DynamicArray();
-        for (int i = 1; i <= 50; i++) {
+        for (int i = 1; i <= 35; i++) {
             ex1.add(i);
         }
-        ex1.print();
+//        ex1.print();
         System.out.println();
-        System.out.println(ex1.getByIndex(11));
-        System.out.println(ex1.getByIndex(-11));
+//        System.out.println(ex1.getByIndex(11));
+//        System.out.println(ex1.getByIndex(-11));
+
+//        ex1.deleteByIndex(16);
+//        System.out.println();
+//        ex1.deleteByIndex(-16);
+//        ex1.deleteByIndex(56);
+//        ex1.deleteByIndex(8);
+//        ex1.set(1, 55);
+//        ex1.addElem(1,55);
+//        System.out.println();
+//        ex1.addElem(16, 99);
+//        System.out.println(ex1.exists(70));
+//        System.out.println(ex1.exists(7));
+        System.out.println(ex1.getIndexByValue(7));
 
     }
 }
